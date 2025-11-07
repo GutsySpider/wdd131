5e62737077b11c1e2fa3ca995d82a28e8c5fd3d8
GutsySpider <bowman.p.michael@gmail.com>
2025-11-07 12:46:12 -0800

    Added styles and scripts

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,3 +3,13 @@ const today = new Date();
 year.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>`;
 
 document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const mainnav = document.querySelector('.navigation');
+const title = document.querySelector('.title');
+
+hamButton.addEventListener('click', () => {
+	mainnav.classList.toggle('open');
+    hamButton.classList.toggle('open');
+    title.classList.toggle('hidden');
+});
